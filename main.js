bee0afe9683daf42d12163bd5f8c1e46a17b3845
purin-time <cocroni.purin@gmail.com>
2021-10-19 15:04:58 +0900

    Update copied section

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 'use strict';
-{
+
   const colorCode = document.getElementById('color-code');
   const colorBg = document.getElementById('bg-color');
   const colorSt = document.getElementById('color_3');
   const colorLg = document.getElementById('color_6');
-  
+  {
   const colorn = [0,3,6,9,"C","F"];
   const array = [];
 
@@ -12,10 +12,6 @@
     const colorR = colorn[Math.floor(Math.random() * colorn.length)];
     array.push(colorR);
   }
-
-  // console.log(isNaN(array[1]));
-  // console.log(array);
-  // console.log(array.join(''));
 
   if (isNaN(array[1])) {
     colorBg.style.color = "#333";
@@ -25,24 +21,36 @@
 
   function colorArray() {
     colorCode.innerHTML = array.join('');
+
+    const colorArrayLg = ["#", array[0], array[0], array[1], array[1], array[2], array[2]];
+    const colorHtLg = colorArrayLg.join('');
+    colorLg.value = colorHtLg;
+    console.log();
+
+    const newArray = ["#", ...array];
+    const colorHt = newArray.join('');
+    colorBg.style.background = colorHt;
+    colorSt.value = colorHt;
   }
   colorArray();
-
-  // const newArr = array.map(colorArrayLg);
-  // console.log(newArr);
-
-  function colorBgStyle() {
-    array.unshift("#");
-    const colorHt = array.join('');
-    colorBg.style.background = colorHt;
-    colorSt.innerHTML = colorHt;
-  }
-  colorBgStyle();
-
 
   const colorCg = document.getElementById("color-change");
   colorCg.onclick = function() {
     location.reload();
-  };
+  }; 
 
+}
+
+function CopyToClipboard1() {
+  colorSt.select();
+  document.execCommand("Copy");
+
+  alert("copied! : " + colorSt.value);
+}
+
+function CopyToClipboard2() {
+  colorLg.select();
+  document.execCommand("Copy");
+
+  alert("copied! : " + colorLg.value);
 }
