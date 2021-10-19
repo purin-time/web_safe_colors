8858026df66c640c4ac5ec93faa1efaacf171798
purin-time <cocroni.purin@gmail.com>
2021-10-19 11:52:24 +0900

    Update copy section

--- main.js
+++ main.js
@@ -2,6 +2,8 @@
 {
   const colorCode = document.getElementById('color-code');
   const colorBg = document.getElementById('bg-color');
+  const colorSt = document.getElementById('color_3');
+  const colorLg = document.getElementById('color_6');
   
   const colorn = [0,3,6,9,"C","F"];
   const array = [];
@@ -11,9 +13,9 @@
     array.push(colorR);
   }
 
-  console.log(isNaN(array[1]));
-  console.log(array);
-  console.log(array.join(''));
+  // console.log(isNaN(array[1]));
+  // console.log(array);
+  // console.log(array.join(''));
 
   if (isNaN(array[1])) {
     colorBg.style.color = "#333";
@@ -21,11 +23,22 @@
     colorBg.style.color = "#fff";
   }
 
+  function colorArray() {
+    colorCode.innerHTML = array.join('');
+  }
+  colorArray();
 
-  colorCode.innerHTML = array.join('');
-  array.unshift("#");
+  // const newArr = array.map(colorArrayLg);
+  // console.log(newArr);
 
-  colorBg.style.background = array.join('');
+  function colorBgStyle() {
+    array.unshift("#");
+    const colorHt = array.join('');
+    colorBg.style.background = colorHt;
+    colorSt.innerHTML = colorHt;
+  }
+  colorBgStyle();
+
 
   const colorCg = document.getElementById("color-change");
   colorCg.onclick = function() {
